05c06d3cbd5d27206d58e1f65b841eadc8b0b8b1
Mel_v <Mvvento@gmail.com>
2024-05-10 18:29:14 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,20 @@
 
 /*     Shop btn     */
 // Create the shop button element
+
+
+// Wait for the DOM content to be fully loaded
 document.addEventListener("DOMContentLoaded", function() {
+  // Get a reference to the button element
   const shopButton = document.getElementById('shop');
 
-  if (shopButton) {
-    // Set the button text
-    shopButton.textContent = "Enter Shop";
-
-    // Set the button link (assuming you want it to link to contact.html)
-    shopButton.href = "contact.html";
-  } else {
-    console.error("Shop button element with ID 'shop' not found!");
-  }
+  // Add a click event listener to the button
+  shopButton.addEventListener('click', function() {
+    // This function will be executed when the button is clicked
+    console.log('Button clicked!');
+    // Add your desired functionality here, such as navigating to another page
+     window.location.href = 'product1.html'; 
+  });
 });
+
 
